b560d8dbabb940f0e1453c24980b3b55ec0d5671
gurox12 <teguhiz173@gmail.com>
2020-09-30 14:54:48 +0700

    project pertama

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,348 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>COVpedia.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/jquery-ui.css">
+  <link rel="stylesheet" href="/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="/css/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="/css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="/css/bootstrap-datepicker.css">
+
+  <link rel="stylesheet" href="/fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="/css/aos.css">
+
+  <link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+
+  <div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+
+    
+    <header class="site-navbar light js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+
+          <div class="col-6 col-xl-2">
+            <div class="mb-0 site-logo"><a href="/" class="mb-0">COVpedia<span class="text-primary">.</span> </a></div>
+          </div>
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="/" class="nav-link">Home</a></li>
+               
+                
+
+
+                <li><a href="/blog" class="active nav-link">Blog</a></li>
+                <li><a href="/about" class="nav-link">About</a></li>
+                <li><a href="/contact" class="nav-link">Contact</a></li>
+
+                <li class="social"><a href="https://www.facebook.com/teguh.i.zakaria/" class="nav-link"><span class="icon-facebook"></span></a></li>
+                <li class="social"><a href="https://www.instagram.com/teguh_iz/" class="nav-link"><span class="icon-instagram"></span></a></li>
+                <li class="social"><a href="/loged" class="nav-link"><span class="icon-person"></span></a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3 text-white"></span></a></div>
+
+        </div>
+      </div>
+
+    </header>
+
+
+    <!-- MAIN -->
+
+
+
+    <div class="slide-item overlay" style="background-image: url('/images/1.jpg');background-attachment: fixed;">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-lg-6 align-self-center">
+            <h1 class="heading mb-3">Blog us</h1>
+            <p class="lead text-white mb-5">Cari tahu tentang kami di sini</p>
+            <p><a href="#" class="btn btn-primary">Get In Touch</a></p>
+          </div>
+        </div>
+      </div>  
+    </div>
+
+
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-7 text-center">
+            <div class="heading">
+              <h2 class="text-black">Portfolio</h2>
+            </div>
+            <p>Galeri Rumah Sakit Di Daerah Kediri Jawa Timur</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="blog-entry">
+              <a href="#" class="d-block">
+                <img src="/images/2.jpg" alt="Image" class="img-fluid">
+              </a>
+              <div class="post-meta d-flex justify-content-center">
+                <span>
+                  <span class="icon-calendar"></span>
+                  <span>23 Jul</span>
+                </span>
+                <span>
+                  <span class="icon-user"></span>
+                  <span>Admin</span>
+                </span>
+                <span>
+                  <span class="icon-comment"></span>
+                  <span>2 Comments</span>
+                </span>
+              </div>
+              <h2><a href="#">Rumah Sakit Bhayangkara Kediri</a></h2>
+              <p>Jl. Kombes Pol Duryat No.17, Dandangan, Kec. Kota Kediri, Kota Kediri, Jawa Timur 64122</p>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="blog-entry">
+              <a href="#" class="d-block">
+                <img src="/images/3.jpg" alt="Image" class="img-fluid">
+              </a>
+              <div class="post-meta d-flex justify-content-center">
+                <span>
+                  <span class="icon-calendar"></span>
+                  <span>23 Jul</span>
+                </span>
+                <span>
+                  <span class="icon-user"></span>
+                  <span>Admin</span>
+                </span>
+                <span>
+                  <span class="icon-comment"></span>
+                  <span>2 Comments</span>
+                </span>
+              </div>
+              <h2><a href="#">RSUD Gambiran Kota Kediri</a></h2>
+              <p>Jl. Kapten Tendean No.16, Pakunden, Kec. Pesantren, Kota Kediri, Jawa Timur 64133</p>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="blog-entry">
+              <a href="#" class="d-block">
+                <img src="/images/4.jpg" alt="Image" class="img-fluid">
+              </a>
+              <div class="post-meta d-flex justify-content-center">
+                <span>
+                  <span class="icon-calendar"></span>
+                  <span>23 Jul</span>
+                </span>
+                <span>
+                  <span class="icon-user"></span>
+                  <span>Admin</span>
+                </span>
+                <span>
+                  <span class="icon-comment"></span>
+                  <span>2 Comments</span>
+                </span>
+              </div>
+              <h2><a href="#">RS Baptis Kediri</a></h2>
+              <p>Jl. Brigjend Pol. IBH Pranoto No.1-7, Bangsal, Kec. Pesantren, Kota Kediri, Jawa Timur 64131</p>
+            </div>
+          </div>
+
+
+          <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="blog-entry">
+              <a href="#" class="d-block">
+                <img src="/images/5.jpg" alt="Image" class="img-fluid">
+              </a>
+              <div class="post-meta d-flex justify-content-center">
+                <span>
+                  <span class="icon-calendar"></span>
+                  <span>23 Jul</span>
+                </span>
+                <span>
+                  <span class="icon-user"></span>
+                  <span>Admin</span>
+                </span>
+                <span>
+                  <span class="icon-comment"></span>
+                  <span>2 Comments</span>
+                </span>
+              </div>
+              <h2><a href="#">RS Ratih Kota Kediri</a></h2>
+              <p>Jl. Penanggungan, Bandar Lor, Kec. Mojoroto, Kota Kediri, Jawa Timur 64114</p>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="blog-entry">
+              <a href="#" class="d-block">
+                <img src="/images/6.jpg" alt="Image" class="img-fluid">
+              </a>
+              <div class="post-meta d-flex justify-content-center">
+                <span>
+                  <span class="icon-calendar"></span>
+                  <span>23 Jul</span>
+                </span>
+                <span>
+                  <span class="icon-user"></span>
+                  <span>Admin</span>
+                </span>
+                <span>
+                  <span class="icon-comment"></span>
+                  <span>2 Comments</span>
+                </span>
+              </div>
+              <h2><a href="#">RS Muhammadiyah Ahmad Dahlan</a></h2>
+              <p>Jl. Gatot Subroto No.84, Mrican, Kec. Mojoroto, Kediri, Jawa Timur 64112</p>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="blog-entry">
+              <a href="#" class="d-block">
+                <img src="/images/7.jpg" alt="Image" class="img-fluid">
+              </a>
+              <div class="post-meta d-flex justify-content-center">
+                <span>
+                  <span class="icon-calendar"></span>
+                  <span>23 Jul</span>
+                </span>
+                <span>
+                  <span class="icon-user"></span>
+                  <span>Admin</span>
+                </span>
+                <span>
+                  <span class="icon-comment"></span>
+                  <span>2 Comments</span>
+                </span>
+              </div>
+              <h2><a href="#">Rumah Sakit Ibu Dan Anak Citra Keluarga</a></h2>
+              <p>Jalan Jenderal Jl. Urip Sumoharjo No.189, Ngronggo, Kec. Kota Kediri, Kota Kediri, Jawa Timur 64129</p>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+
+    
+
+
+    <div class="site-footer bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <h2 class="footer-heading mb-4">About</h2>
+          <p>Web setatis ini di buat uantuk memenuhi tugas dari PT.Lontar Jaya. Yaitu, Membuat sebuah aplikasi web Covid-19. Yang bertujuan untuk memudahkan masyarakat dalam mengakses informasi covid-19. </p>
+          <div class="my-5 social">
+            <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+            <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+            <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="row">
+            <div class="col-lg-4">
+              <h2 class="footer-heading mb-4">Create by</h2>
+              <ul class="list-unstyled">
+                <li><a href="#">Laptop ASUS</a></li>
+                <li><a href="#">Laravel-x8</a></li>
+                <li><a href="#">Sublime</a></li>
+                <li><a href="#">Command Prompt</a></li>
+                <li><a href="#">Google Chrome</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4">
+              <h2 class="footer-heading mb-4">Company</h2>
+              <ul class="list-unstyled">
+                <li><a href="#">SMK Al-ittihad</a></li>
+                <li><a href="#">PKL-New normal</a></li>
+                <li><a href="#">PT.Lontar Jaya</a></li>
+                <li><a href="#">Santri Altie</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4">
+              <h2 class="footer-heading mb-4">Navigation</h2>
+              <ul class="list-unstyled">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-12">
+          <div class="border-top pt-5">
+            <p class="copyright"><small>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;<script>document.write(new Date().getFullYear());</script> COVpedia | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="#" target="_blank" >TIZ</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <script src="/js/jquery-3.3.1.min.js"></script>
+  <script src="/js/jquery-ui.js"></script>
+  <script src="/js/popper.min.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
+  <script src="/js/owl.carousel.min.js"></script>
+  <script src="/js/jquery.countdown.min.js"></script>
+  <script src="/js/jquery.easing.1.3.js"></script>
+  <script src="/js/aos.js"></script>
+  <script src="/js/jquery.waypoints.min.js"></script>
+  <script src="/js/jquery.animateNumber.min.js"></script>
+  <script src="/js/jquery.fancybox.min.js"></script>
+  <script src="/js/jquery.sticky.js"></script>
+  <script src="/js/isotope.pkgd.min.js"></script>
+
+
+  <script src="/js/main.js"></script>
+
+
+</body>
+</html>
